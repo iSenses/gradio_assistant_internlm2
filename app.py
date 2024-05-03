dc5bf76dff6a562fa555c329030837f4dbff538b
nachtmusik <nachtmusik@163.com>
2024-05-03 11:16:29 +0800

    Revert "fixed no cuda error"
    This reverts commit f8ae3743c554a163c8be49d7aa80b207636fd2c8.

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ os.system(f'git clone https://code.openxlab.org.cn/mingyanglee/assistant-1_8b.gi
 os.system(f'cd {base_path} && git lfs pull')
 
 tokenizer = AutoTokenizer.from_pretrained(base_path,trust_remote_code=True)
-model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, device_map='auto', torch_dtype=torch.float16) # .cuda()
+model = AutoModelForCausalLM.from_pretrained(base_path,trust_remote_code=True, torch_dtype=torch.float16).cuda()
 
 def chat(message,history):
     for response,history in model.stream_chat(tokenizer,message,history,max_length=2048,top_p=0.8,temperature=1):
